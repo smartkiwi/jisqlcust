6cca6f4815b71a85c8986368dd8865548293b65e
vvlad <vvladymy@telcordia.com>
2011-02-04 15:05:26 -0500

    Initial version

---
+++ src/com/xigole/util/sql/Version.java
@@ -0,0 +1,6 @@
+package com.xigole.util.sql;
+/* this class is auto-generated during an ant build */
+public class Version {
+    public static final String version = "2.0.8.422";
+    public static final String buildDate = "Jan 12, 2011 at 09:45:50 AM MST";
+}
